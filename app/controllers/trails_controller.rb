b994eb6deb8ff9ab11fe3ba54f8c80f05e1558f7
Nathan Park <nate.park90@gmail.com>
2016-01-23 15:12:23 -0800

    Add post route to write trails to database

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -6,7 +6,19 @@ class TrailsController < ApplicationController
   end
   
   def create
-    Trail.new(name: , distance: ,)    
-    render json: params
+    name = params[:name]
+    length = params[:trailGeoStats][:length]
+    elevation_gain = params[:trailGeoStats][:elevation_gain]
+    trail = Trail.find_or_initialize_by(name: name, length: length, elevation_gain: elevation_gain)
+    
+    if trail.new_record?
+      trail.save
+      render nothing: true
+    elsif trail.persisted?
+      render nothing: true
+    else
+      render status: 401
+    end
   end
+
 end
